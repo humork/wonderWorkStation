9f6a8c81b04b8fe49b7bc4c9aa081bd05458e514
jiaowodashujiaowoemo <chenxiang950926@gmail.com>
2018-07-03 15:15:39 +0800

    DrivingRecDaoImpl.java

--- src/com/cc/dao/impl/DrivingRecDaoImpl.java
+++ src/com/cc/dao/impl/DrivingRecDaoImpl.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Repository;
 
 import com.cc.dao.IDrivingRecDao;
 import com.cc.po.DrivingRec;
-
+/**
+ * @Author ChenXiang
+ * @Date 2018/6/30,10:58
+ */
 @Repository("Rec")
 public class DrivingRecDaoImpl extends BaseDaoImpl<DrivingRec, Integer> implements IDrivingRecDao {
 

0af2674e762b1581d5f50ae92464c8493700bd79
[wh0228] <[649671445@qq.com]>
2018-07-03 12:48:37 +0800

    commit utils

--- src/com/cc/util/JsonUtil.java
+++ src/com/cc/util/JsonUtil.java
@@ -1,47 +1,51 @@
 package com.cc.util;
 
-
-
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.PropertyFilter;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 
-
 public class JsonUtil {
 	/**
 	 * 响应回json对象
+	 * 
 	 * @param object
 	 */
-	public static String writeJson(Object object){
-		String json=JSON.toJSONString(object);
-		System.out.println(json+"json");
+	public static String writeJson(Object object) {
+		String json = JSON.toJSONString(object);
+		System.out.println(json + "json");
 		return json;
 	}
+
 	/***
 	 * 响应json,并过滤不需要的属性
-	 * @param object 待转换对象
-	 * @param propertyNames 字符串数组结构：不想转换的属性，例：new String[]{"pro_1","pro_2"}
+	 * 
+	 * @param object
+	 *            待转换对象
+	 * @param propertyNames
+	 *            字符串数组结构：不想转换的属性，例：new String[]{"pro_1","pro_2"}
 	 */
-	public static String writeJson(Object object,String... propertyNames) {
-		String json=null;
+	public static String writeJson(Object object, String... propertyNames) {
+		String jsonRes = null;
 		try {
-			//过滤属性
-			PropertyFilter pf= filterProperty(propertyNames);
-			//套用过滤规则.加上这个可以把$ref过滤掉！
-			json=JSON.toJSONString(object, pf, SerializerFeature.DisableCircularReferenceDetect);
-			System.out.println(json+"json");
+			// 过滤属性
+			PropertyFilter pf = filterProperty(propertyNames);
+			// 套用过滤规则.加上这个可以把$ref过滤掉！
+			jsonRes = JSON.toJSONString(object, pf, SerializerFeature.DisableCircularReferenceDetect);
+			System.out.println(jsonRes + "json");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return json;
+		return jsonRes;
 	}
-	
+
 	/***
 	 * 属性过滤器
-	 * @param propertyNames 不需要转换的属性
+	 * 
+	 * @param propertyNames
+	 *            不需要转换的属性
 	 * @return
 	 */
-	public static  PropertyFilter filterProperty(final String... propertyNames) {
+	public static PropertyFilter filterProperty(final String... propertyNames) {
 		// 过滤不显示的属性及关联属性
 		PropertyFilter propertyFilter = new PropertyFilter() {
 
@@ -57,5 +61,5 @@ public class JsonUtil {
 			}
 		};
 		return propertyFilter;
-	}	
+	}
 }
--- src/com/cc/util/SysStr.java
+++ src/com/cc/util/SysStr.java
@@ -2,37 +2,37 @@ package com.cc.util;
 
 //公用提示[简体中文]
 public class SysStr {
-	//树菜单最子节点主键的长度
-	public final static int maxlen=6;
-	
-	//系统管理员登录名
-	public final static String admin="admin";
-	
-	public final static String addsuccess="新增成功";
-	public final static String addfail="新增失败";
-	
-	public final static String backsuccess="回车成功";
-	public final static String backfail="回车失败";
-	
-	public final static String getsuccess="取车成功";
-	public final static String getfail="取车失败";
-	
-	public final static String uploadsuccess="上传成功";
-	public final static String uploadfail="上传失败";
-	
-	public final static String editsuccess="修改成功";
-	public final static String editfail="修改失败";
-	
-	public final static String delsuccess="删除成功";
-	public final static String delfail="删除失败";
-	
-	public final static String grantsuccess="授权成功";
-	public final static String grantfail="授权失败";
-	
-	public final static String assignrolessuccess="分配角色成功";
-	public final static String assignrolesfail="分配角色失败";
-	
-	public final static String error="未知错误";
-	public final static String syserror="系统错误";
-	
+	// 树菜单最子节点主键的长度
+	public final static int maxlen = 5;
+
+	// 系统管理员登录名
+	public final static String admin = "admin";
+
+	public final static String addsuccess = "新增成功";
+	public final static String addfail = "新增失败";
+
+	public final static String backsuccess = "回车成功";
+	public final static String backfail = "回车失败";
+
+	public final static String getsuccess = "取车成功";
+	public final static String getfail = "取车失败";
+
+	public final static String uploadsuccess = "上传成功";
+	public final static String uploadfail = "上传失败";
+
+	public final static String editsuccess = "修改成功";
+	public final static String editfail = "修改失败";
+
+	public final static String delsuccess = "删除成功";
+	public final static String delfail = "删除失败";
+
+	public final static String grantsuccess = "授权成功";
+	public final static String grantfail = "授权失败";
+
+	public final static String assignrolessuccess = "分配角色成功";
+	public final static String assignrolesfail = "分配角色失败";
+
+	public final static String error = "未知错误";
+	public final static String syserror = "系统错误";
+
 }

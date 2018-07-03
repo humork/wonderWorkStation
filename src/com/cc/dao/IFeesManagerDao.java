ca692d7950b73b2520c08ab81e45b239e6ae92d0
jiaowodashujiaowoemo <chenxiang950926@gmail.com>
2018-07-03 15:13:56 +0800

    IFeesManagerDao.java

--- src/com/cc/dao/IFeesManagerDao.java
+++ src/com/cc/dao/IFeesManagerDao.java
@@ -1,7 +1,10 @@
 package com.cc.dao;
 
 import com.cc.po.FeesManager;
-
+/**
+ * @Author ChenXiang
+ * @Date 2018/6/29,11:10
+ */
 public interface IFeesManagerDao extends IBaseDao<FeesManager,Integer>{
 	
 }

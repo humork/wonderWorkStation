7e0fdc7110368de23e51e395ed91e07add57f5a3
jiaowodashujiaowoemo <chenxiang950926@gmail.com>
2018-07-03 15:13:23 +0800

    IDrivingRecDao.java

--- src/com/cc/dao/IDrivingRecDao.java
+++ src/com/cc/dao/IDrivingRecDao.java
@@ -1,7 +1,10 @@
 package com.cc.dao;
 
 import com.cc.po.DrivingRec;
-
+/**
+ * @Author ChenXiang
+ * @Date 2018/6/29,11:02
+ */
 public interface IDrivingRecDao extends IBaseDao<DrivingRec,Integer> {
 	
 }

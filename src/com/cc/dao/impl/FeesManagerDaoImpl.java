c0fd177bd89a68013a0b5a6b7a95755c23dde917
jiaowodashujiaowoemo <chenxiang950926@gmail.com>
2018-07-03 15:16:04 +0800

    FeesManagerDaoImpl.java

--- src/com/cc/dao/impl/FeesManagerDaoImpl.java
+++ src/com/cc/dao/impl/FeesManagerDaoImpl.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Repository;
 
 import com.cc.dao.IFeesManagerDao;
 import com.cc.po.FeesManager;
+/**
+ * @Author ChenXiang
+ * @Date 2018/6/30,11:00
+ */
 @Repository("Fees")
 public class FeesManagerDaoImpl extends BaseDaoImpl<FeesManager,Integer> implements IFeesManagerDao {
 

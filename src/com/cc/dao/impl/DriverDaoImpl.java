f5a2a8cb31fcb1b4de059d6c7658a9ef9fc1e0e8
jiaowodashujiaowoemo <chenxiang950926@gmail.com>
2018-07-03 15:15:17 +0800

    DriverDaoImpl.java

--- src/com/cc/dao/impl/DriverDaoImpl.java
+++ src/com/cc/dao/impl/DriverDaoImpl.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Repository;
 import com.cc.dao.IDriverDao;
 import com.cc.po.Driver;
 
+/**
+ * @Author ChenXiang
+ * @Date 2018/6/30,10:57
+ */
 @Repository("Driver")
 public class DriverDaoImpl extends BaseDaoImpl<Driver, Integer> implements IDriverDao {
 

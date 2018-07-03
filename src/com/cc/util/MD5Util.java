b76b549983233fba4258b88a71e7df6cc99fbd84
[wh0228] <[649671445@qq.com]>
2018-07-03 12:46:13 +0800

    commit MD5

--- src/com/cc/util/MD5Util.java
+++ src/com/cc/util/MD5Util.java
@@ -2,51 +2,54 @@ package com.cc.util;
 
 import java.security.MessageDigest;
 
-/***
- * MD5加密处理
- * @author roy
- * @time:2016-7-25
+/**
+ * 
+ * @Description:MD5加密处理
+ * @author: wh
+ * @date:2018年5月3日下午5:35:48
  */
 public class MD5Util {
-	
-	 /*** 
-     * MD5加密 生成32位md5码
-     * @param 待加密字符串
-     * @return 返回32位md5码
-     */
-    public static String md5Encode(String inStr) {
-        MessageDigest md5 = null;
-        StringBuffer hexValue = new StringBuffer();
-        
-        try {
-            md5 = MessageDigest.getInstance("MD5");
-            
-            byte[] byteArray = inStr.getBytes("UTF-8");
-            byte[] md5Bytes = md5.digest(byteArray);
-            
-            for (int i = 0; i < md5Bytes.length; i++) {
-                int val = ((int) md5Bytes[i]) & 0xff;
-                if (val < 16) {
-                    hexValue.append("0");
-                }
-                hexValue.append(Integer.toHexString(val));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "";
-        }  
-        
-        return hexValue.toString();
-    }
-    
-    /**
-     * 测试主函数
-     * @param args
-     * @throws Exception
-     */
-    public static void main(String args[]) {
-        String str = new String("love");
-        System.out.println("原始：" + str);
-        System.out.println("MD5后：" + md5Encode(str));
-    }
+
+	/***
+	 * MD5加密 生成32位md5码
+	 * 
+	 * @param 待加密字符串
+	 * @return 返回32位md5码
+	 */
+	public static String md5Encode(String inStr) {
+		MessageDigest md5 = null;
+		StringBuffer hexValue = new StringBuffer();
+
+		try {
+			md5 = MessageDigest.getInstance("MD5");
+
+			byte[] byteArray = inStr.getBytes("UTF-8");
+			byte[] md5Bytes = md5.digest(byteArray);
+
+			for (int i = 0; i < md5Bytes.length; i++) {
+				int val = ((int) md5Bytes[i]) & 0xff;
+				if (val < 16) {
+					hexValue.append("0");
+				}
+				hexValue.append(Integer.toHexString(val));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "";
+		}
+
+		return hexValue.toString();
+	}
+
+	/**
+	 * 测试主函数
+	 * 
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String args[]) {
+		String str = new String("love");
+		System.out.println("原始：" + str);
+		System.out.println("MD5后：" + md5Encode(str));
+	}
 }
